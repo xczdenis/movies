e5bf41dd39b4bd03a93ed83146cba56716733514
Denis <dfgaraev@yandex.ru>
2022-08-29 09:27:38 +0000

    build: добавлены линтеры и конфиги к ним

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hello world')
+import time
+
+
+def my_print(msg):
+    """
+    sdfsadf
+    sdf
+    sadfsadf
+    """
+    time.sleep(1)
+    print(msg)
+
+
+my_print("hello")
+
+b = {"a": 10, "b": 15}
