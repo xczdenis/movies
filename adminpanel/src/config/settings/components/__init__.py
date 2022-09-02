7ae56bca18269d703c9c870ff1a3de51866c005c
Denis <dfgaraev@yandex.ru>
2022-09-02 16:13:10 +0300

    feat: добавлен сервис adminpanel и ETL sqlite_to_pg

--- adminpanel/src/config/settings/components/__init__.py
+++ adminpanel/src/config/settings/components/__init__.py
@@ -4,7 +4,7 @@ from config import env_manager
 from decouple import AutoConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent.parent
 
 path_to_env_file = ROOT_DIR / ".envs" / env_manager.get_env()
--- etl/src/transformers/es/person.py
+++ etl/src/transformers/es/person.py
@@ -1,23 +1,23 @@
-from dataclasses import dataclass
-from typing import Optional
-
-from loguru import logger
-from pg_to_es.models.es import Person
-from pg_to_es.transformers.base import ESBaseTransformer
-
-
-@dataclass
-class PersonTransformer(ESBaseTransformer):
-    index_name: str
-
-    def transform_row(self, row_data) -> Optional[Person]:
-        try:
-            return Person.parse_obj(row_data)
-        except Exception as e:
-            logger.error(
-                "An error occurred during person data transformation. ID: {id}. Error: {error}".format(
-                    id=row_data.get("id"), error=e
-                )
-            )
-
-        return None
+# from dataclasses import dataclass
+# from typing import Optional
+#
+# from loguru import logger
+# from pg_to_es.models.es import Person
+# from pg_to_es.transformers.base import ESBaseTransformer
+#
+#
+# @dataclass
+# class PersonTransformer(ESBaseTransformer):
+#     index_name: str
+#
+#     def transform_row(self, row_data) -> Optional[Person]:
+#         try:
+#             return Person.parse_obj(row_data)
+#         except Exception as e:
+#             logger.error(
+#                 "An error occurred during person data transformation. ID: {id}. Error: {error}".format(
+#                     id=row_data.get("id"), error=e
+#                 )
+#             )
+#
+#         return None
