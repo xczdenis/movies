03ac00ba16f2bf4fa64b40102de75a537e13b78e
Denis <dfgaraev@yandex.ru>
2022-09-04 06:58:44 +0300

    feat: добавлены ETL процессы (sqlite-to-pg и pg-to-es)

--- etl/src/pg_to_es.py
+++ etl/src/pg_to_es.py
@@ -1,9 +1,10 @@
+from processes.processes import PGToESProcess
 from clients.es import ESClient
 from clients.pg import PGClient
 from config import config
 from decorators import backoff
 from helpers import create_es_indexes
-from processes.processes import PGToESProcess
+
 
 es_client = ESClient("{host}:{port}".format(host=config.ELASTIC_HOST, port=config.ELASTIC_PORT))
 pg_client = PGClient(
