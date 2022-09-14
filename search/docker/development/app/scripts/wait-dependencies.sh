ff8e6855519725dc026b58889fc45ad8b7c747a5
Denis <xczdenis@gmail.com>
2022-09-14 09:35:40 +0300

    fix: увеличен таймаут ожидания сервисов до 120 секунд

--- search/docker/development/app/scripts/wait-dependencies.sh
+++ search/docker/development/app/scripts/wait-dependencies.sh
@@ -3,12 +3,12 @@ set -e
 
 # Redis
 echo "\033[94mWaiting the service: \033[97mRedis (url=$REDIS_HOST:$REDIS_PORT)\033[00m"
-/wait-for-it.sh $REDIS_HOST:$REDIS_PORT --
+/wait-for-it.sh $REDIS_HOST:$REDIS_PORT -t 120 --
 echo "\033[01;32mRedis is up!\033[00m"
 
 # Elasticsearch
 echo "\033[94mWaiting the service: \033[97mElasticsearch (url=$ELASTIC_HOST:$ELASTIC_PORT)\033[00m"
-/wait-for-it.sh $ELASTIC_HOST:$ELASTIC_PORT --
+/wait-for-it.sh $ELASTIC_HOST:$ELASTIC_PORT -t 120 --
 echo "\033[01;32mElasticsearch is up!\033[00m"
 
 exec "$@"
