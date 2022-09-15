ffcfda246746575f05527330e7aeb1e10ffd6387
Denis <xczdenis@gmail.com>
2022-09-15 18:05:24 +0300

    fix: запуск adminpanel в production режиме изменен на gunicorn

--- adminpanel/docker/app/scripts/start.sh
+++ adminpanel/docker/app/scripts/start.sh
@@ -4,5 +4,5 @@ if [ "x$ENVIRONMENT" = 'xdevelopment' ]; then
     python manage.py runserver 0.0.0.0:8000
 else
     echo "\033[94mRun in production mode mode\033[00m"
-    python manage.py runserver 0.0.0.0:8000
+    gunicorn -b 0.0.0.0:8000 config.wsgi:application
 fi
