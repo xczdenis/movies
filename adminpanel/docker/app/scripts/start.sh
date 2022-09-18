61623a173d3fe484d885bc6ca87363294d5a90ec
Denis <xczdenis@gmail.com>
2022-09-18 11:44:35 +0300

    fix: захардкоженные HOST и PORT изменены на переменные окружения

--- adminpanel/docker/app/scripts/start.sh
+++ adminpanel/docker/app/scripts/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "x$ENVIRONMENT" = 'xdevelopment' ]; then
     echo "\033[94mRun in development mode\033[00m"
-    python manage.py runserver 0.0.0.0:8000
+    python manage.py runserver $ADMINPANEL_APP_HOST:$ADMINPANEL_APP_PORT
 else
     echo "\033[94mRun in production mode mode\033[00m"
-    gunicorn -b 0.0.0.0:8000 config.wsgi:application
+    gunicorn -b $ADMINPANEL_APP_HOST:$ADMINPANEL_APP_PORT config.wsgi:application
 fi
