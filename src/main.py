7a3f920a0fa521196d06d4391bc25ba9788b2530
Denis <xczdenis@gmail.com>
2023-05-23 20:02:30 +0300

    ci: добавлены тесты в docker

--- src/main.py
+++ src/main.py
@@ -19,19 +19,40 @@ class ElasticFaker:
         return await self.db_client.indices.exists(index=self.index_name)
 
 
-async def main():
+async def main_http():
     es_client = AsyncElasticsearch(hosts=["http://elasticsearch:9200"], basic_auth=("elastic", "123qwe"))
     # person_faker = ElasticFaker(db_client=es_client, index_name="movies")
     timeout = 20
     print(f"sleep {timeout} sec")
     await asyncio.sleep(timeout)
     response = await es_client.ping()
-    print(f"ping = {response}")
+    print(f"main_http ping = {response}")
 
     # await person_faker.create_index()
     #
     await es_client.close()
 
 
+async def main_https():
+    es_client = AsyncElasticsearch(hosts=["https://elasticsearch:9200"], basic_auth=("elastic", "123qwe"))
+    # person_faker = ElasticFaker(db_client=es_client, index_name="movies")
+    timeout = 20
+    print(f"sleep {timeout} sec")
+    await asyncio.sleep(timeout)
+    response = await es_client.ping()
+    print(f"main_https ping = {response}")
+
+    # await person_faker.create_index()
+    #
+    await es_client.close()
+
+
+async def main():
+    await main_http()
+    await main_https()
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(
+        main(),
+    )
