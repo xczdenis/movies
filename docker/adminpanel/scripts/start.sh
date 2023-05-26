d7bda1f5a063d931942233244f18b6714fd74458
Denis <103623003+xczdenis@users.noreply.github.com>
2023-05-26 15:27:58 +0300

    feat: обновлена документация и добавлены пару фич (#8)
    * chore: добавлен хук для проверки сообщений к коммиту
    
    * docs: обновлена документация
    
    * chore: актуализирован шаблон env
    
    * refactor: удалена папка temp
    
    * fix: исправлена ошибка не позволяющая запустить сервис локально
    
    * chore: gunicorn перенесен в группу main
    
    * refactor: изменены команды для запуска в режиме production
    
    * refactor: рефакторинг docker

--- docker/adminpanel/scripts/start.sh
+++ docker/adminpanel/scripts/start.sh
@@ -6,6 +6,6 @@ if [ "x$ENVIRONMENT" = 'xdevelopment' ]; then
     log_info "Run in development mode"
     python src/"${ADMINPANEL_PKG_NAME}"/manage.py runserver "${ADMINPANEL_APP_HOST}":"${ADMINPANEL_APP_PORT}"
 else
-    echo "\033[94mRun in production mode mode\033[00m"
-    gunicorn -b "${ADMINPANEL_APP_HOST}":"${ADMINPANEL_APP_PORT}" config.wsgi:application
+    log_info "Run in production mode"
+    gunicorn -b "${ADMINPANEL_APP_HOST}":"${ADMINPANEL_APP_PORT}" src."${ADMINPANEL_PKG_NAME}".config.wsgi:application
 fi
